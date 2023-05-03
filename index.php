bf4bf6ddd7301afa684c0756202576f753ed20f9
alessiaBer <alexdeyn@gmail.com>
2023-05-03 15:41:33 +0200

    [add] form markup

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Descrizione:
 Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
@@ -14,13 +14,32 @@ stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <h1 class="text-center py-4">PHP Badwords</h1>
+    <div class="container d-flex justify-content-center">
+        <div class="card rounded-0 col-8 shadow">
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="paragraph" class="form-label">Type your paragraph</label>
+                    <textarea class="form-control" name="paragraph" id="paragraph" rows="3"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="badword" class="form-label">Type the bad word</label>
+                    <input type="text" class="form-control" name="badword" id="badword">
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
