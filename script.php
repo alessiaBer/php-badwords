9d12cf700dc6681189766e34f3ccc17321f7e11a
alessiaBer <alexdeyn@gmail.com>
2023-05-03 16:20:59 +0200

    [print] on screen replaced string

--- script.php
+++ script.php
@@ -1,7 +1,8 @@
 <?php 
 $stringLength = strlen($_POST['paragraph']);
-
+$replacedParagraph = str_replace($_POST["badword"], "***", $_POST["paragraph"]);
 
 echo 'The paragraph is ', $stringLength, ' characters long<br>';
 echo $_POST["paragraph"];
+echo $replacedParagraph;
 
