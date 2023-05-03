992d0e6def1019b8fe9af7b9cb64dd733bf2e1d6
alessiaBer <alexdeyn@gmail.com>
2023-05-03 16:29:02 +0200

    [refactoring & clean] code

--- script.php
+++ script.php
@@ -1,8 +1,10 @@
 <?php 
-$stringLength = strlen($_POST['paragraph']);
-$replacedParagraph = str_replace($_POST["badword"], "***", $_POST["paragraph"]);
+$paragraph = ($_POST["paragraph"]);
+$stringLength = strlen($paragraph);
+$badWord = ($_POST["badword"]);
+$replacedParagraph = str_replace($badWord, "***", $paragraph);
 
 echo 'The paragraph is ', $stringLength, ' characters long<br>';
-echo $_POST["paragraph"];
+echo $paragraph,'<br>';
 echo $replacedParagraph;
 
